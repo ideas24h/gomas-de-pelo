432cfbcb109acd08dfadf6c07815e840f507da81
Raúl <raul@ideas24h.com>
2019-09-30 10:01:17 +0200

    Link en Header

--- src/components/GomaHeader.vue
+++ src/components/GomaHeader.vue
@@ -1,9 +1,9 @@
 <template>
   <header class="mb-8 sm:mb-16 text-black leading-snug">
     <div class="flex items-center flex-shrink-0  mr-6">
-      <span class="font-bold text-4xl tracking-tight">
-        <g-link  to="/">{{ $static.metadata.siteName }}</g-link>
-      </span>
+      <g-link  to="/" class="font-bold bg-pink-500  text-center text-white p-8 m-6 text-4xl tracking-tight">
+        {{ $static.metadata.siteName }}
+      </g-link>
       </div>
     <h2 class="text-3xl sm:text-5xl leading-tight font-bold mb-4 text-black">{{ goma.title }}</h2>
   </header>
